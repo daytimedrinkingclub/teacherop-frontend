2fe7d2eee2e75379ba20329f89df70c281bb9915
TeacherOp <admin@teacherop.com>
2024-06-10 12:39:42 +0530

    updated firebase settings and pointed to a new project

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBPjjsB-4kDSYIZFL0GQirJdwo7LZKIpFs",
-  authDomain: "teacherop-2717c.firebaseapp.com",
-  projectId: "teacherop-2717c",
-  storageBucket: "teacherop-2717c.appspot.com",
-  messagingSenderId: "1078934686379",
-  appId: "1:1078934686379:web:0378fd2ba31af823ce45ee",
-  measurementId: "G-FM17NCKL3F"
+  apiKey: "AIzaSyDwtrh3e0Q2wa11yZLiscZRP6j6P_y2Llw",
+  authDomain: "teacherop-420.firebaseapp.com",
+  projectId: "teacherop-420",
+  storageBucket: "teacherop-420.appspot.com",
+  messagingSenderId: "300743871781",
+  appId: "1:300743871781:web:168123e85fceb855839ecc",
+  measurementId: "G-68NW6E7VVB"
 };
 
 const app = initializeApp(firebaseConfig);
